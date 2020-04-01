132998c8d5abbb004e1f7cde349d848118ff153d
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-04-02 00:09:14 +0900

    message if

--- app.py
+++ app.py
@@ -53,29 +53,45 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.message.text == "ねむい"：
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="さっさと進捗だせ！")
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     try:
+        # メッセージIDを受け取る
         message_id = event.message.id
+        # メッセージIDに含まれるmessage_contentを抽出する
         message_content = line_bot_api.get_message_content(message_id)
+        # contentの画像データをバイナリデータとして扱えるようにする
         image = BytesIO(message_content.content)
 
+        # Detect from streamで顔検出
         detected_faces = face_client.face.detect_with_stream(image)
 
+        # 検出結果に応じて処理を分ける
         if detected_faces != []:
+            # 検出された顔の最初のIDを取得
             text = detected_faces[0].face_id
 
+            # 顔検出ができたら顔認証を行う
             valified = face_client.face.verify_face_to_person(
                 face_id = detected_faces[0].face_id,
                 person_group_id = PERSON_GROUP_ID,
                 person_id = PERSON_ID_HASIKAN
             )
             
+            # 認証結果に応じて処理を変える
             if valified:
                 if valified.is_identical:
                     text = "この写真は橋本環奈です(score:{:.3f})".format(valified.confidence)
@@ -87,6 +103,7 @@ def handle_image(event):
     except:
         text = "エラーが発生しました。"
 
+    # LINEチャネルを通じてメッセージを返答
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text)

d98f982fe7a5c4c466da4b51fe8e22c46006da5f
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-04-04 01:43:53 +0900

    debug

--- app.py
+++ app.py
@@ -58,12 +58,12 @@ def callback():
 
         if params["flag"] == 1:
             print(params["name_list"])
-            name = face_client.person_group_person.create(
-                    person_group_id = PERSON_GROUP_ID,
-                    name = params["name_list"][0]
-                )
-            person_id_name = name.person_id
-            params["person_id_name"].append(person_id_name)
+            # name = face_client.person_group_person.create(
+            #         person_group_id = PERSON_GROUP_ID,
+            #         name = params["name_list"][0]
+            #     )
+            # person_id_name = name.person_id
+            # params["person_id_name"].append(person_id_name)
             print(params["person_id_name"])
             params["text_list"][0] = "null" 
             params["flag"] = 0
